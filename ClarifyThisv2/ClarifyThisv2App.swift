37c2a9f964d13b872eadb32f401e3756f49e4394
Thanush <thanushgithub@gmail.com>
2025-12-27 17:51:42 +0400

    Initial Commit

---
+++ ClarifyThisv2/ClarifyThisv2App.swift
@@ -0,0 +1,17 @@
+//
+//  ClarifyThisv2App.swift
+//  ClarifyThisv2
+//
+//  Created by Thanush Manchikanti on 27/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ClarifyThisv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClarifyThisv2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClarifyThisv2
+//
+//  Created by Thanush Manchikanti on 27/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
